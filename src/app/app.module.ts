9cec7b6b76c95d1da223f99f608cb57fc05f247b
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-10-18 10:26:53 +0700

    update simple calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import {AppComponent} from './app.component';
+import {FontSizeEditorComponent} from './font-size-editor/font-size-editor.component';
+import {PetComponent} from './pet/pet.component';
+import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './product/product.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FontSizeEditorComponent
+    FontSizeEditorComponent,
+    PetComponent,
+    ProductComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
